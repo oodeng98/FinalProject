6c015efd33400fed294b45c23d6c8fcb301f731a
cndwnw213 <cndwn213@naver.com>
2022-07-21 23:54:45 +0900

    새로고침 및 이미지 src 정리

--- flask/static/index_script.js
+++ flask/static/index_script.js
@@ -1,9 +1,6 @@
 
 // ---------------- search 페이지 ------------------------------------------------
 // ------------------------------------------------------------------------------
-const search_box = document.getElementById("search_box")
-const keyword_box = document.getElementsByClassName("keyword_box")[0]
-const search_cancle = document.getElementsByClassName("search_cancle")[0]
 const story_boxs = document.getElementsByClassName("story_box")
 const story_viewer = document.getElementsByClassName('story_viewer')[0]
 const story_left_button = document.getElementsByClassName('left_button')[0]
@@ -13,86 +10,6 @@ function heart_click(){
     alert("♥");
 }
 
-// 검색 부분
-search_cancle.onclick = function(){ // input에 text 있을 때 취소 버튼 누르면 초기화
-        search_box.value = '';
-    }    
-search_box.onfocus = function(){ // input에 포커스 있을 경우 모양 변화
-    this.style.backgroundImage = "None"
-    search_cancle.style.opacity = "100"
-    search_cancle.style.display = "block"
-}
-search_box.onblur = function(){// input에 포커스 사라질 경우 모양 변화
-    search_cancle.style.opacity = "0"
-    search_box.style.backgroundImage = "url(../static/img/glass.png)"
-    setTimeout(function(){
-        search_cancle.style.display = "none"
-        // input에 포커스 사라질 시, 검색어 박스 사라지도록
-        keyword_box.style.display = "none"
-    },100)
-}
-search_box.oninput = function(){
-    // 검색 박스에 값이 없다면 종료
-    if(search_box.value =="") return;
-    // 글씨 칠 때 마다 검색 박스 초기화
-    keyword_box.removeChild(document.getElementsByClassName("keyword_outer")[0])
-    var new_box = document.createElement('div')
-    new_box.className = 'keyword_outer'
-    keyword_box.appendChild(new_box)
-    // 검색박스 드러내기
-    keyword_box.style.display = "flex"
-    // 비동기 통신(관련 검색어 가져오기)
-    $.ajax({
-            type:"POST",
-            url:"/keywords",
-            data:{'keyword':search_box.value},
-            dataType : "json",
-            success: function(keywords){
-                if(Object.keys(keywords) ==0){return}
-                // 검색어 갯수만큼 div 박스 생성, 검색한 텍스트 넣어주기
-                for(i = 0; i < Object.keys(keywords).length; i++){
-                    var keyword_outer = document.getElementsByClassName('keyword_outer')[0]
-                    var a = document.createElement("a");
-                    var div1 = document.createElement("div");
-                    var div2 = document.createElement("div");
-                    var div3 = document.createElement("div");
-                    var div4 = document.createElement("div");
-                    var div5 = document.createElement("div");
-                    var span1 = document.createElement("span");
-                    var span2 = document.createElement("span");
-                    var sharp = document.createElement("img")
-
-                    // 검색페이지 링크 설정 
-                    // a.href = "/search?keyword=바다"
-                    a.href = keywords[String(i+1)]
-                    sharp.src = "../static/img/sharp.png"
-                    span1.innerText = keywords[String(i+1)]
-                    span2.innerText = "게시물"+ String(1000000) + "개"
-                    // span2.innerText = keywords["게시물"+ keywords[i+1] + "개"]
-
-                    div1.className = "keywords"
-                    div2.className = "sharp_img"
-                    div3.className = "key_text_box"
-                    div4.className = "keywords_name"
-                    div5.className = "keywords_counts"
-                    
-                    div4.appendChild(span1)
-                    div5.appendChild(span2)
-                    div3.appendChild(div4)
-                    div3.appendChild(div5)
-                    div2.append(sharp)
-                    div1.append(div2)
-                    div1.append(div3)
-                    a.append(div1)
-                    div1.href = "/"
-                    keyword_outer.appendChild(a)
-                }
-            },
-            error: function(result){
-                console.log("failed")
-            }
-        })
-}
 
 // 스토리 뷰어 길이 설정
 story_viewer_width = Number(window.getComputedStyle(story_viewer).width.slice(0,-2));
@@ -120,8 +37,7 @@ story_left_button.onclick = function(){
 
 
 
-// ---------------- search 페이지 ------------------------------------------------
-// ------------------------------------------------------------------------------
+
 
 
 
--- flask/static/search_script.js
+++ flask/static/search_script.js
@@ -1,87 +1,4 @@
-// 검색 부분
-const search_box = document.getElementById("search_box")
-const keyword_box = document.getElementsByClassName("keyword_box")[0]
-const search_cancle = document.getElementsByClassName("search_cancle")[0]
-search_cancle.onclick = function(){ // input에 text 있을 때 취소 버튼 누르면 초기화
-    search_box.value = '';
-}    
-search_box.onfocus = function(){ // input에 포커스 있을 경우 모양 변화
-this.style.backgroundImage = "None"
-search_cancle.style.opacity = "100"
-search_cancle.style.display = "block"
-}
-search_box.onblur = function(){// input에 포커스 사라질 경우 모양 변화
-search_cancle.style.opacity = "0"
-search_box.style.backgroundImage = "url(../static/img/glass.png)"
-setTimeout(function(){
-    search_cancle.style.display = "none"
-    // input에 포커스 사라질 시, 검색어 박스 사라지도록
-    keyword_box.style.display = "none"
-},100)
-}
-search_box.oninput = function(){
-// 검색 박스에 값이 없다면 종료
-if(search_box.value =="") return;
-// 글씨 칠 때 마다 검색 박스 초기화
-keyword_box.removeChild(document.getElementsByClassName("keyword_outer")[0])
-var new_box = document.createElement('div')
-new_box.className = 'keyword_outer'
-keyword_box.appendChild(new_box)
-// 검색박스 드러내기
-keyword_box.style.display = "flex"
-// 비동기 통신(관련 검색어 가져오기)
-$.ajax({
-        type:"POST",
-        url:"/keywords",
-        data:{'keyword':search_box.value},
-        dataType : "json",
-        success: function(keywords){
-            if(Object.keys(keywords) ==0){return}
-            // 검색어 갯수만큼 div 박스 생성, 검색한 텍스트 넣어주기
-            for(i = 0; i < Object.keys(keywords).length; i++){
-                var keyword_outer = document.getElementsByClassName('keyword_outer')[0]
-                var a = document.createElement("a");
-                var div1 = document.createElement("div");
-                var div2 = document.createElement("div");
-                var div3 = document.createElement("div");
-                var div4 = document.createElement("div");
-                var div5 = document.createElement("div");
-                var span1 = document.createElement("span");
-                var span2 = document.createElement("span");
-                var sharp = document.createElement("img")
-
-                // 검색페이지 링크 설정 
-                a.href = "/search?keyword=바다"
-                // a.href = keywords[String(i+1)]
-                sharp.src = "../static/img/sharp.png"
-                span1.innerText = keywords[String(i+1)]
-                span2.innerText = "게시물"+ String(1000000) + "개"
-                // span2.innerText = keywords["게시물"+ keywords[i+1] + "개"]
-
-                div1.className = "keywords"
-                div2.className = "sharp_img"
-                div3.className = "key_text_box"
-                div4.className = "keywords_name"
-                div5.className = "keywords_counts"
-                
-                div4.appendChild(span1)
-                div5.appendChild(span2)
-                div3.appendChild(div4)
-                div3.appendChild(div5)
-                div2.append(sharp)
-                div1.append(div2)
-                div1.append(div3)
-                a.append(div1)
-                div1.href = "/"
-                keyword_outer.appendChild(a)
-            }
-        },
-        error: function(result){
-            console.log("failed")
-        }
-    })
-}
-
+// 이미지를 누르면
 const img_box = document.getElementsByClassName('recently_img_box')
 for(var i = 0; i < img_box.length; i++){
     img_box[i].onclick = function(){
@@ -89,8 +6,50 @@ for(var i = 0; i < img_box.length; i++){
     };
 };
 
-
-
-
-
-
+// 스크롤 내리면 로딩
+window.addEventListener("scroll", function(){
+	var top =  window.scrollY // 모든 브라우저 지원
+			 || window.pageYOffset // explorer9 미만에서 지원x
+			 || document.documentElement.scrollTop // explorer8이하에서 지원 but chrome에서 사용 x
+			 || document.body.scrollTop; // chrome, safari, opera, edge 사용 가능
+    var main = this.document.getElementsByTagName('html')[0]
+    var height = Number(main.scrollHeight)
+    var height2 = Number(main.scrollTop)
+    var height3 = Number(main.clientHeight)
+    if(height - height2 < height3+20){
+        var recently_img_box = document.getElementsByClassName('recently_img_box')
+        var keyword_profile_name_keyword = this.document.getElementById('keyword_profile_name_keyword')
+        var num = String(recently_img_box.length)
+        data = {'keyword':keyword_profile_name_keyword.innerText.slice(1), 'num': num}
+        $.ajax({
+            type:"POST",
+            contentType: "application/json; charset=utf-8",
+            url:"/image_append",
+            data:JSON.stringify(data),
+            dataType : "json",
+            success: function(image_names){
+                if(Object.keys(image_names) ==0){return}
+                // 검색어 갯수만큼 div 박스 생성, 검색한 텍스트 넣어주기
+                var recently_img_container = document.getElementsByClassName('recently_img_container')[0]
+                for(i = 0; i < Object.keys(image_names).length; i++){
+                    if(i%3 == 0){
+                        if(i != 0){
+                            recently_img_container.appendChild(row_box);
+                         }
+                        var row_box = document.createElement("div")
+                        row_box.className = "recently_img_row_box"
+                    }
+                    var img_box = document.createElement("div")
+                    var img_ = document.createElement("img")
+                    img_box.className = 'recently_img_box'
+                    img_.src = "../static/img/"+Object.values(image_names[i])
+                    img_box.appendChild(img_)
+                    row_box.appendChild(img_box)
+                }
+            },
+            error: function(result){
+                console.log("failed")
+            }
+            })
+    }
+})
